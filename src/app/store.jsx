a00101af3461c46d7cb0f6eea4872dc6690e9400
ShauryaKAJLA <126277304+ShauryaKAJLA@users.noreply.github.com>
2024-03-20 09:44:48 +0530

    Delete src directory

--- src/app/store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import CartReducer from '../pages/cart/CartSlice'
-
-export const store= configureStore({
-    reducer:{
-        cart:CartReducer,
-    }
-}) 
